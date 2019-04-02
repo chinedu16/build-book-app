d87e5e58eac6b7ebbfd1780a60655df703a3d69d
Chinedu Ohagwu <chinedu.ohagwu@gmail.com>
2019-04-02 18:43:08 +0100

    create header components

--- src/components/Partials/Header.vue
+++ src/components/Partials/Header.vue
@@ -1,12 +1,9 @@
 <template>
-    <div class="main-header">
-        <a href="#default" class="logo">CompanyLogo</a>
-        <div class="header-right">
-            <a class="active" href="#home">Home</a>
-            <a href="#contact">Contact</a>
-            <a href="#about">About</a>
-        </div>
-    </div>
+    <header class="header-fixed">
+      <div class="header-limiter">
+        <h1><a href="#">Book<span>App</span></a></h1>
+      </div>
+    </header>
 </template>
 
 <script>
@@ -15,10 +12,78 @@ export default {
 }
 </script>
 
-<style scoped>
+<style>
+.header-fixed {
+  margin: -7px;
+  background: rgb(255,128,170);
+  background: linear-gradient(90deg, rgba(255,128,170,1) 0%, rgba(255,194,102,1) 100%);
+	/* background-color:  rgba(38, 132, 240, 0.8); */
+	box-shadow:0 1px 1px #ccc;
+	padding: 20px 40px;
+	height: 80px;
+	color: #ffffff;
+	box-sizing: border-box;
+	top:-100px;
 
-.main-header {
-  background-color: #f1f1f1;
-  padding: 20px 10px;
+	-webkit-transition:top 0.3s;
+	transition:top 0.3s;
 }
+
+.header-fixed .header-limiter {
+	max-width: 1200px;
+	text-align: center;
+	margin: 0 auto;
+}
+
+.header-fixed-placeholder{
+	height: 80px;
+	display: none;
+}
+
+/* Logo */
+
+.header-fixed .header-limiter h1 {
+	float: left;
+	font: normal 28px Cookie, Arial, Helvetica, sans-serif;
+	line-height: 40px;
+	margin: 0;
+}
+
+.header-fixed .header-limiter h1 span {
+	color: #2c3e50;
+}
+
+/* The navigation links */
+
+.header-fixed .header-limiter a {
+	color: #ffffff;
+	text-decoration: none;
+}
+
+.header-fixed .header-limiter nav {
+	font:16px Arial, Helvetica, sans-serif;
+	line-height: 40px;
+	float: right;
+}
+
+.header-fixed .header-limiter nav a{
+	display: inline-block;
+	padding: 0 5px;
+	text-decoration:none;
+	color: #ffffff;
+	opacity: 0.9;
+}
+
+/* Fixed version of the header */
+
+body.fixed .header-fixed {
+	padding: 10px 40px;
+	height: 50px;
+	position: fixed;
+	width: 100%;
+	top: 0;
+	left: 0;
+	z-index: 1;
+}
+
 </style>
