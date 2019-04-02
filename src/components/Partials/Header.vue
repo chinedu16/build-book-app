31573400a7948b9ed9b1fc6762a7793123bf9f3b
Chinedu Ohagwu <chinedu.ohagwu@gmail.com>
2019-04-02 18:11:15 +0100

    vue template containers

---
+++ src/components/Partials/Header.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="main-header">
+        <a href="#default" class="logo">CompanyLogo</a>
+        <div class="header-right">
+            <a class="active" href="#home">Home</a>
+            <a href="#contact">Contact</a>
+            <a href="#about">About</a>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+.main-header {
+  background-color: #f1f1f1;
+  padding: 20px 10px;
+}
+</style>
